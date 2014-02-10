346543d6f955f6f3aff4fb54f4e5c2d3829897e3
Timothy Gu <timothygu99@gmail.com>
2014-02-10 03:05:45 +0000

    License stuff

--- test.c
+++ test.c
@@ -1,19 +1,20 @@
 /*
  * dlfcn-win32
- * Copyright (c) 2007 Ramiro Polla
+ * Copyright (c) 2007-2009 Ramiro Polla
+ * Copyright (c) 2014      Tiancheng "Timothy" Gu
  *
- * This library is free software; you can redistribute it and/or
+ * dlfcn-win32 is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * dlfcn-win32 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
+ * License along with dlfcn-win32; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
